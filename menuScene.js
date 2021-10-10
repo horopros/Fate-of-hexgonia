46644f9e23c5acda85ce3e4979880994210fe0c0
Filippo <fmonteleo.ne904@gmail.com>
2021-10-11 00:37:04 +0200

    feat: new background and new font

--- menuScene.js
+++ menuScene.js
@@ -5,14 +5,19 @@ export default class menuScene extends Phaser.Scene {
 
   preload() {
     this.load.image('button', 'assets/button.png');
+    this.load.image('background', 'assets/bkg.png');
   }
 
   create() {
+    const background = this.add.image(0, 0, 'background');
+    background.setOrigin(0);
     const container = this.add.container(200, 100);
     const image = this.add.image(0, 0, 'button');
     image.setOrigin(0);
     const text = this.add.text(image.displayWidth / 2, image.displayHeight / 2, 'Play', {
       fontSize: 48,
+      fontFamily: 'TypeReg',
+      color: 'black',
     });
     text.setOrigin(0.5);
     container.add(image);
@@ -21,7 +26,7 @@ export default class menuScene extends Phaser.Scene {
       new Phaser.Geom.Rectangle(image.x, image.y, image.displayWidth, image.displayHeight),
       Phaser.Geom.Rectangle.Contains,
     );
-    this.input.enableDebug(container);
+    // this.input.enableDebug(container);
     container.on(
       'pointerdown',
       function () {
